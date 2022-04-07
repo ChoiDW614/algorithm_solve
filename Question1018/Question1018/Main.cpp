9ce14eb18ba0639ab9e0231ef137dfe192a32809
ChoiDW614 <81404753+ChoiDW614@users.noreply.github.com>
2022-04-07 15:19:32 +0900

    modify file name

--- Question1018/Question1018/Main.cpp
+++ Question1018/Question1018/Main.cpp
@@ -9,11 +9,9 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	
+
 	cin >> H >> W;
 	vector<vector<char>> chessBoard(H, vector<char>(W, 0));
 
-
 	return 0;
-
 }
